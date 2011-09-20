b4e5ab4aa343c50ab2dda1eab3ed35985c8ceebd
HNJSlater <henry.slater@bcs.org>
2011-09-21 00:10:49 +0100

    Basic AJAX interface now...

--- src/main/java/webservice/webservice.java
+++ src/main/java/webservice/webservice.java
@@ -1,7 +1,10 @@
 package webservice;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -36,239 +39,283 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 public class webservice  {
 
-    private model.Game game;
+	private model.Game game;
 
-    public webservice(Game game) {
-	this.game = game;
-    }
-
-    public void handleRoot(HttpExchange t) throws IOException {
-	ByteArrayOutputStream response = new ByteArrayOutputStream();
-
-	OutputStream os = t.getResponseBody();       
-
-	try {
-
-	    TransformerHandler hd = createTransformHandler(response);
-	    AttributesImpl atts = new AttributesImpl();
-	    hd.startElement("","","game",atts);
-	    atts.addAttribute("","","winning_victory_points","CDATA",Integer.toString(game.getWinningVP()));
-	    hd.startElement("","","rules", atts);
-	    hd.endElement("", "", "rules");
-	    atts.clear();
-	    generatePlayersXML(hd);
-	    hd.endElement("","","game");
-	    hd.endDocument();
-
-	    t.sendResponseHeaders(200, response.size());
-	    response.writeTo(os); 
-	    os.close();        
+	public webservice(Game game) {
+		this.game = game;
 	}
-	catch (Exception e) {
 
-	    sendErrorMessage(500, t, e.getMessage());    
-	}
-    }
+	public void handleRoot(HttpExchange t) throws IOException {
+		ByteArrayOutputStream response = new ByteArrayOutputStream();
 
-    private void generatePlayersXML(TransformerHandler hd)
-    throws SAXException {
+		OutputStream os = t.getResponseBody();       
 
-	AttributesImpl atts = new AttributesImpl();
-	hd.startElement("", "", "players", atts);
-	for (Player p : game.getLeaderBoard())
-	{
-	    generatePlayerXML(hd, p);
-	}
-	hd.endElement("", "", "players");
-    }
+		try {
 
-    public void handlePlayers(HttpExchange t) throws IOException {
-	ByteArrayOutputStream response = new ByteArrayOutputStream();
+			TransformerHandler hd = createTransformHandler(response);
+			AttributesImpl atts = new AttributesImpl();
+			hd.startElement("","","game",atts);
+			atts.addAttribute("","","winning_victory_points","CDATA",Integer.toString(game.getWinningVP()));
+			hd.startElement("","","rules", atts);
+			hd.endElement("", "", "rules");
+			atts.clear();
+			generatePlayersXML(hd);
+			hd.endElement("","","game");
+			hd.endDocument();
 
-	OutputStream os = t.getResponseBody();     
-
-	String method = t.getRequestMethod().toLowerCase();
-
-	try {
-	    TransformerHandler hd = createTransformHandler(response);
-
-	    // /players
-	    if (t.getRequestURI().getPath().equals("/players") || t.getRequestURI().getPath().equals("/players/")) {
-		generatePlayersXML(hd);
-	    }
-	    // /players/COLOR
-	    else if (t.getRequestURI().getPath().matches("^/players/[^/]*/{0,1}$")){
-
-		PlayerColor playercolor = PlayerColor.valueOf(t.getRequestURI().getPath().split("/")[2]);
-		Player player = game.getPlayer(playercolor);
-
-		if (method.equals("post") || method.equals("put")) {
-		    updatePlayer(t, player);		    
+			t.sendResponseHeaders(200, response.size());
+			response.writeTo(os); 
+			os.close();        
 		}
-		else if (method.equals("get")) {
-		    Player p = game.getPlayer(playercolor);
-		    generatePlayerXML(hd, p);
+		catch (Exception e) {
+
+			sendErrorMessage(500, t, e.getMessage());    
 		}
-		else {
-		    sendErrorMessage(405, t, "Bad Method");
-		    return;
-		}
-	    }
-	    
-	    // /players/COLOR/achievements
-	    else if (t.getRequestURI().getPath().matches("^/players/[^/]*/achievements/{0,1}$")) {
-
-		PlayerColor playercolor = PlayerColor.valueOf(t.getRequestURI().getPath().split("/")[2]);
-		Player player = game.getPlayer(playercolor);
-
-		generatePlayerAchievementsXML(hd, player);
-
-	    }
-	    
-	    // /players/COLOR/achievements/ACHIEVEMENT
-	    else if (t.getRequestURI().getPath().matches("^/players/[^/]*/achievements/[^/]+{0,1}$")) {
-
-		String[] path = t.getRequestURI().getPath().split("/");
-		PlayerColor playercolor = PlayerColor.valueOf(path[2]);
-		Achievement achievement = Achievement.valueOf(path[4]);
-
-		if (method.equals("put") || method.equals("post"))
-		    game.getPlayer(playercolor).add(achievement);
-		else if (method.equals("delete"))
-		    game.getPlayer(playercolor).remove(achievement);
-		else {
-		    sendErrorMessage(405, t, "Bad Method");
-		    return;
-		}
-	    }
-	    else {
-		sendErrorMessage(404, t, "Resource Not Found");
-		return;
-	    }
-	    hd.endDocument();
-
-	    t.sendResponseHeaders(200, response.size());
-	    response.writeTo(os); 
-	    os.close();
-	}
-	catch (Exception e) {
-	    //e.printStackTrace(new PrintStream(response));
-
-	    sendErrorMessage(500, t, e.getMessage());    
 	}
 
-    }
+	private void generatePlayersXML(TransformerHandler hd)
+	throws SAXException {
 
-    private void updatePlayer(HttpExchange t, Player player) throws ParserConfigurationException, SAXException,
-	    IOException, RulesBrokenException {
-	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	DocumentBuilder db = dbf.newDocumentBuilder();
-	Document dom = db.parse(t.getRequestBody());
+		AttributesImpl atts = new AttributesImpl();
+		hd.startElement("", "", "players", atts);
+		for (Player p : game.getLeaderBoard())
+		{
+			generatePlayerXML(hd, p);
+		}
+		hd.endElement("", "", "players");
+	}
 
-	if (dom.getDocumentElement().hasAttribute("name"))
-	player.setName(dom.getDocumentElement().getAttribute("name"));
+	public void handlePlayers(HttpExchange t) throws IOException {
+		ByteArrayOutputStream response = new ByteArrayOutputStream();
 
-	if (dom.getDocumentElement().hasAttribute("settlement_points"))
-	player.setVP(Integer.parseInt(dom.getDocumentElement().getAttribute("settlement_points")));
+		OutputStream os = t.getResponseBody();     
 
-	List<Achievement> incoming_achievements = new ArrayList<Achievement>();
-	NodeList player_children = dom.getDocumentElement().getChildNodes();
-	if (player_children.getLength() > 0) {
-	    NodeList achievements = player_children.item(0).getChildNodes();
-	    
-		for (int i = 0; i < achievements.getLength(); i++) {
-		    Node achievement = achievements.item(i);
-		    incoming_achievements.add(Achievement.valueOf(achievement.getAttributes().getNamedItem("name").getNodeValue()));
+		String method = t.getRequestMethod().toLowerCase();
 
+		try {
+			TransformerHandler hd = createTransformHandler(response);
+
+			// /players
+			if (t.getRequestURI().getPath().equals("/players") || t.getRequestURI().getPath().equals("/players/")) {
+				generatePlayersXML(hd);
+			}
+			// /players/COLOR
+			else if (t.getRequestURI().getPath().matches("^/players/[^/]*/{0,1}$")){
+
+				PlayerColor playercolor = PlayerColor.valueOf(t.getRequestURI().getPath().split("/")[2]);
+				Player player = game.getPlayer(playercolor);
+
+				if (method.equals("post") || method.equals("put")) {
+					updatePlayer(t, player);		    
+				}
+				else if (method.equals("get")) {
+					Player p = game.getPlayer(playercolor);
+					generatePlayerXML(hd, p);
+				}
+				else {
+					sendErrorMessage(405, t, "Bad Method");
+					return;
+				}
+			}
+
+			// /players/COLOR/achievements
+			else if (t.getRequestURI().getPath().matches("^/players/[^/]*/achievements/{0,1}$")) {
+
+				PlayerColor playercolor = PlayerColor.valueOf(t.getRequestURI().getPath().split("/")[2]);
+				Player player = game.getPlayer(playercolor);
+
+				generatePlayerAchievementsXML(hd, player);
+
+			}
+
+			// /players/COLOR/achievements/ACHIEVEMENT
+			else if (t.getRequestURI().getPath().matches("^/players/[^/]*/achievements/[^/]+{0,1}$")) {
+
+				String[] path = t.getRequestURI().getPath().split("/");
+				PlayerColor playercolor = PlayerColor.valueOf(path[2]);
+				Achievement achievement = Achievement.valueOf(path[4]);
+
+				if (method.equals("put") || method.equals("post"))
+					game.getPlayer(playercolor).add(achievement);
+				else if (method.equals("delete"))
+					game.getPlayer(playercolor).remove(achievement);
+				else {
+					sendErrorMessage(405, t, "Bad Method");
+					return;
+				}
+			}
+			else {
+				sendErrorMessage(404, t, "Resource Not Found");
+				return;
+			}
+			hd.endDocument();
+
+			t.sendResponseHeaders(200, response.size());
+			response.writeTo(os); 
+			os.close();
+		}
+		catch (Exception e) {
+			//e.printStackTrace(new PrintStream(response));
+
+			sendErrorMessage(500, t, e.getMessage());    
 		}
 
-	    for (Achievement a : player.getAchievements()) {
-		if (!incoming_achievements.contains(a))
-		    player.remove(a);
-	    }
-
-	    for (Achievement a : incoming_achievements) {
-		player.add(a);
-	    }
 	}
-    }
 
-    private void sendErrorMessage(int code, HttpExchange t, String message) throws IOException {
-	ByteArrayOutputStream response = new ByteArrayOutputStream();
-	(new PrintWriter(response)).write(message);
-	t.sendResponseHeaders(code, response.size());
+	private void updatePlayer(HttpExchange t, Player player) throws ParserConfigurationException, SAXException,
+	IOException, RulesBrokenException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document dom = db.parse(t.getRequestBody());
 
-	OutputStream os = t.getResponseBody(); 
-	response.writeTo(os);
-	os.close();
-    }
+		if (dom.getDocumentElement().hasAttribute("name"))
+			player.setName(dom.getDocumentElement().getAttribute("name"));
 
-    private void generatePlayerXML(TransformerHandler hd, Player p)
-    throws SAXException {
-	AttributesImpl atts = new AttributesImpl();
+		if (dom.getDocumentElement().hasAttribute("settlement_points"))
+			player.setVP(Integer.parseInt(dom.getDocumentElement().getAttribute("settlement_points")));
 
-	atts.clear();
-	atts.addAttribute("","","name","CDATA",p.getName());
-	atts.addAttribute("","","color","CDATA",p.getPlayerColor().toString());
-	atts.addAttribute("", "", "victory_points", "CDATA", Integer.toString(p.getVP()));
-	hd.startElement("","","player",atts);
+		List<Achievement> incoming_achievements = new ArrayList<Achievement>();
+		NodeList player_children = dom.getDocumentElement().getChildNodes();
+		if (player_children.getLength() > 0) {
+			NodeList achievements = player_children.item(0).getChildNodes();
 
-	atts.clear();
-	generatePlayerAchievementsXML(hd, p);
-	hd.endElement("","","player");
-    }
+			for (int i = 0; i < achievements.getLength(); i++) {
+				Node achievement = achievements.item(i);
+				incoming_achievements.add(Achievement.valueOf(achievement.getAttributes().getNamedItem("name").getNodeValue()));
 
-    private void generatePlayerAchievementsXML(TransformerHandler hd, Player p) throws SAXException {
-	AttributesImpl atts = new AttributesImpl();
-	hd.startElement("", "", "achievements", atts);
+			}
 
-	for (Achievement a : p.getAchievements()) {
-	    atts.clear();
-	    atts.addAttribute("", "", "name", "CDATA", a.toString());
+			for (Achievement a : player.getAchievements()) {
+				if (!incoming_achievements.contains(a))
+					player.remove(a);
+			}
 
-
-	    hd.startElement("", "", "achievement", atts);
-	    hd.endElement("", "", "achievement");
+			for (Achievement a : incoming_achievements) {
+				player.add(a);
+			}
+		}
 	}
-	hd.endElement("", "", "achievements");
-    }
 
-    private TransformerHandler createTransformHandler(
-	    ByteArrayOutputStream response)
-    throws TransformerFactoryConfigurationError,
-    TransformerConfigurationException, SAXException {
-	PrintWriter out = new PrintWriter(response);
-	StreamResult streamResult = new StreamResult(out);
-	SAXTransformerFactory tf = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
-	// SAX2.0 ContentHandler.
-	TransformerHandler hd = tf.newTransformerHandler();
-	Transformer serializer = hd.getTransformer();
-	serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
-	serializer.setOutputProperty(OutputKeys.INDENT,"yes");
-	hd.setResult(streamResult);
-	hd.startDocument();
-	return hd;
-    }
+	private void sendErrorMessage(int code, HttpExchange t, String message) {
+		try {
+		ByteArrayOutputStream response = new ByteArrayOutputStream();
+		PrintWriter p = new PrintWriter(response);
+		p.write(message);
+		p.close();
+		t.sendResponseHeaders(code, response.size());
 
-    public void start() throws IOException {
-	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-	server.createContext("/", new HttpHandler() {
-	    @Override
-	    public void handle(HttpExchange arg0) throws IOException {
-		handleRoot(arg0);		
-	    }	       
-	});
-	server.createContext("/players", new HttpHandler() {
-	    @Override
-	    public void handle(HttpExchange arg0) throws IOException {
-		handlePlayers(arg0);		
-	    }	       
-	});
+		OutputStream os = t.getResponseBody(); 
+		response.writeTo(os);
+		os.close();
+		}
+		catch (Exception ex) {
+			t.close();
+		
+		}
+	}
+
+	private void generatePlayerXML(TransformerHandler hd, Player p)
+	throws SAXException {
+		AttributesImpl atts = new AttributesImpl();
+
+		atts.clear();
+		atts.addAttribute("","","name","CDATA",p.getName());
+		atts.addAttribute("","","color","CDATA",p.getPlayerColor().toString());
+		atts.addAttribute("", "", "victory_points", "CDATA", Integer.toString(p.getSettlementVP()));
+		hd.startElement("","","player",atts);
+
+		atts.clear();
+		generatePlayerAchievementsXML(hd, p);
+		hd.endElement("","","player");
+	}
+
+	private void generatePlayerAchievementsXML(TransformerHandler hd, Player p) throws SAXException {
+		AttributesImpl atts = new AttributesImpl();
+		hd.startElement("", "", "achievements", atts);
+
+		for (Achievement a : p.getAchievements()) {
+			atts.clear();
+			atts.addAttribute("", "", "name", "CDATA", a.toString());
 
 
-	server.setExecutor(null); // creates a default executor
-	server.start();
-    }
+			hd.startElement("", "", "achievement", atts);
+			hd.endElement("", "", "achievement");
+		}
+		hd.endElement("", "", "achievements");
+	}
+
+	private TransformerHandler createTransformHandler(
+			ByteArrayOutputStream response)
+	throws TransformerFactoryConfigurationError,
+	TransformerConfigurationException, SAXException {
+		PrintWriter out = new PrintWriter(response);
+		StreamResult streamResult = new StreamResult(out);
+		SAXTransformerFactory tf = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
+		// SAX2.0 ContentHandler.
+		TransformerHandler hd = tf.newTransformerHandler();
+		Transformer serializer = hd.getTransformer();
+		serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
+		serializer.setOutputProperty(OutputKeys.INDENT,"yes");
+		hd.setResult(streamResult);
+		hd.startDocument();
+		return hd;
+	}
+
+	public void start() throws IOException {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		server.createContext("/", new HttpHandler() {
+			@Override
+			public void handle(HttpExchange arg0) throws IOException {
+				handleRoot(arg0);		
+			}	       
+		});
+		server.createContext("/players", new HttpHandler() {
+			@Override
+			public void handle(HttpExchange arg0) throws IOException {
+				handlePlayers(arg0);		
+			}	       
+		});
+		server.createContext("/gui", new HttpHandler() {
+			@Override
+			public void handle(HttpExchange arg0) throws IOException {
+				handleGui(arg0);		
+			}	       
+		});
+
+		server.setExecutor(null); // creates a default executor
+		server.start();
+	}
+
+	protected void handleGui(HttpExchange t) {
+		// TODO Auto-generated method stub
+		try {
+
+			ByteArrayOutputStream response = new ByteArrayOutputStream();
+			PrintWriter p = new PrintWriter(response);
+			InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("gui.html");
+			if (is == null)
+				throw new RuntimeException("Couldn't find resource");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String line;
+			
+            while ((line = br.readLine()) != null) {
+                //Process the data, here we just print it out
+                p.write(line + "\n");
+            }
+
+			p.close();
+			
+			t.sendResponseHeaders(200, response.size());
+
+			OutputStream os = t.getResponseBody(); 
+			response.writeTo(os);
+			os.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.fillInStackTrace();
+			sendErrorMessage(500, t, e.getMessage());
+		}
+	}
 }
 
 
