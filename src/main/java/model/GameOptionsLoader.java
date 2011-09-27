22aa3d3142972d45f201485bb7302395d6250fb1
HNJSlater <henry.slater@bcs.org>
2011-09-27 21:43:14 +0100

    Use the XPath lib in javac not the com.sun... one.

--- src/main/java/model/GameOptionsLoader.java
+++ src/main/java/model/GameOptionsLoader.java
@@ -10,14 +10,18 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-import org.w3c.dom.traversal.NodeIterator;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.sun.org.apache.xpath.internal.XPathAPI;
+
 
 
 public class GameOptionsLoader {
@@ -25,21 +29,21 @@ public class GameOptionsLoader {
 		
 	}
 	
-	public static GameOptions load() throws ParserConfigurationException, SAXException, IOException, TransformerException {
+	public static GameOptions load() throws ParserConfigurationException, SAXException, IOException, TransformerException, XPathException {
 		Set<Achievement> achievements = new HashSet<Achievement>();
 		Set<PlayerColor> playerColors = new HashSet<PlayerColor>();
 		
-
 		InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("game.xml");
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document dom = db.parse(is);
-		dom.getDocumentElement().normalize();
-		
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		Document document = builder.parse(is);
 
-		NodeIterator nl = XPathAPI.selectNodeIterator(dom.getDocumentElement(), "/game/achievements/achievement");
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		String expression = "/game/achievements/achievement";
+		NodeList nodes = (NodeList) xpath.evaluate(expression, document, XPathConstants.NODESET);
+		
 		Node node;
-		while ((node = nl.nextNode()) != null) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			node = nodes.item(i);
 			NamedNodeMap attrs = node.getAttributes();
 			String name = attrs.getNamedItem("name").getNodeValue();
 			int victory_points = Integer.parseInt(attrs.getNamedItem("victory_points").getNodeValue());
@@ -48,15 +52,16 @@ public class GameOptionsLoader {
 			achievements.add(new Achievement(name, victory_points, short_name, character));
 		}
 		
-		nl = XPathAPI.selectNodeIterator(dom.getDocumentElement(), "/game/colors/color");
-		while ((node = nl.nextNode()) != null) {
+		expression = "/game/colors/color";
+		nodes = (NodeList) xpath.evaluate(expression, document, XPathConstants.NODESET);
+		for (int i = 0; i < nodes.getLength(); i++) {
+			node = nodes.item(i);		
 			NamedNodeMap attrs = node.getAttributes();
 			String name = attrs.getNamedItem("name").getNodeValue();
 			String hex_value = attrs.getNamedItem("hex_value").getNodeValue();
 			char character = attrs.getNamedItem("character").getNodeValue().toCharArray()[0];
 			playerColors.add(new PlayerColor(name, Color.decode(hex_value), character));
 		}
-		
 		return new GameOptions(achievements, playerColors);
 	}
 }
